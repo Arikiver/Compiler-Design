935501468a3158c34229c79ee17b380eeca9d7ca
Aaradhya Bhatiya <Bhatiyaaaradhya@gmail.com>
2025-02-12 02:18:59 +0530

    new file:   Lab2/comment_counter2.l 	new file:   Lab2/count.exe 	new file:   Lab2/count.l 	modified:   Lab2/file_stats.exe 	modified:   Lab2/lex.yy.c 	new file:   Lab2/test

--- Lab2/lex.yy.c
+++ Lab2/lex.yy.c
@@ -282,30 +282,29 @@ static void yy_fatal_error YY_PROTO(( yyconst char msg[] ));
 	*yy_cp = '\0'; \
 	yy_c_buf_p = yy_cp;
 
-#define YY_NUM_RULES 8
-#define YY_END_OF_BUFFER 9
-static yyconst short int yy_accept[17] =
+#define YY_NUM_RULES 5
+#define YY_END_OF_BUFFER 6
+static yyconst short int yy_accept[10] =
     {   0,
-        0,    0,    0,    0,    9,    7,    6,    7,    5,    3,
-        5,    2,    1,    4,    1,    0
+        0,    0,    6,    4,    1,    3,    2,    2,    0
     } ;
 
 static yyconst int yy_ec[256] =
     {   0,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    2,
+        1,    1,    1,    1,    1,    1,    1,    1,    2,    3,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    3,    1,    1,    1,    1,    4,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
+        1,    2,    1,    1,    1,    1,    1,    1,    1,    1,
+        1,    1,    1,    1,    1,    1,    1,    4,    4,    4,
+        4,    4,    4,    4,    4,    4,    4,    1,    1,    1,
+        1,    1,    1,    1,    4,    4,    4,    4,    4,    4,
+        4,    4,    4,    4,    4,    4,    4,    4,    4,    4,
+        4,    4,    4,    4,    4,    4,    4,    4,    4,    4,
+        1,    1,    1,    1,    1,    1,    4,    4,    4,    4,
 
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
+        4,    4,    4,    4,    4,    4,    4,    4,    4,    4,
+        4,    4,    4,    4,    4,    4,    4,    4,    4,    4,
+        4,    4,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
@@ -324,31 +323,29 @@ static yyconst int yy_ec[256] =
 
 static yyconst int yy_meta[5] =
     {   0,
-        1,    2,    1,    3
+        1,    1,    1,    2
     } ;
 
-static yyconst short int yy_base[19] =
+static yyconst short int yy_base[11] =
     {   0,
-        0,    0,    4,    0,   14,   15,   15,    6,   15,   15,
-        0,   15,    0,   15,    0,   15,    9,   10
+        0,    0,    6,    7,    7,    7,    0,    0,    7,    3
     } ;
 
-static yyconst short int yy_def[19] =
+static yyconst short int yy_def[11] =
     {   0,
-       16,    1,   16,    3,   16,   16,   16,   16,   16,   16,
-       17,   16,   18,   16,   18,    0,   16,   16
+        9,    1,    9,    9,    9,    9,   10,   10,    0,    9
     } ;
 
-static yyconst short int yy_nxt[20] =
+static yyconst short int yy_nxt[12] =
     {   0,
-        6,    7,    6,    8,    9,   10,   11,    9,   12,   13,
-       15,   14,   15,   16,    5,   16,   16,   16,   16
+        4,    5,    6,    7,    8,    9,    3,    9,    9,    9,
+        9
     } ;
 
-static yyconst short int yy_chk[20] =
+static yyconst short int yy_chk[12] =
     {   0,
-        1,    1,    1,    1,    3,    3,    3,    3,    8,    8,
-       18,   17,   18,    5,   16,   16,   16,   16,   16
+        1,    1,    1,    1,   10,    3,    9,    9,    9,    9,
+        9
     } ;
 
 static yy_state_type yy_last_accepting_state;
@@ -362,16 +359,11 @@ static char *yy_last_accepting_cpos;
 #define YY_MORE_ADJ 0
 #define YY_RESTORE_YY_MORE_OFFSET
 char *yytext;
-#line 1 "comment_counter.l"
+#line 1 "file_stats.l"
 #define INITIAL 0
-#line 2 "comment_counter.l"
-    int single_line_comments = 0;    // Count of single-line comments
-    int multi_line_comments = 0;     // Count of lines in multi-line comments
-    int in_comment = 0;              // Flag to track if we're inside a multi-line comment
-    int comment_line_start = 0;      // Flag to track if current line started a multi-line comment
-#define MULTI_COMMENT 1
-
-#line 375 "lex.yy.c"
+#line 2 "file_stats.l"
+    int characters = 0, words = 0, spaces = 0, lines = 0;
+#line 367 "lex.yy.c"
 
 /* Macros after this point can all be overridden by user definitions in
  * section 1.
@@ -522,9 +514,9 @@ YY_DECL
 	register char *yy_cp, *yy_bp;
 	register int yy_act;
 
-#line 10 "comment_counter.l"
+#line 5 "file_stats.l"
 
-#line 528 "lex.yy.c"
+#line 520 "lex.yy.c"
 
 	if ( yy_init )
 		{
@@ -575,13 +567,13 @@ yy_match:
 			while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 				{
 				yy_current_state = (int) yy_def[yy_current_state];
-				if ( yy_current_state >= 17 )
+				if ( yy_current_state >= 10 )
 					yy_c = yy_meta[(unsigned int) yy_c];
 				}
 			yy_current_state = yy_nxt[yy_base[yy_current_state] + (unsigned int) yy_c];
 			++yy_cp;
 			}
-		while ( yy_base[yy_current_state] != 15 );
+		while ( yy_base[yy_current_state] != 7 );
 
 yy_find_action:
 		yy_act = yy_accept[yy_current_state];
@@ -609,63 +601,31 @@ do_action:	/* This label is used only to access EOF actions. */
 
 case 1:
 YY_RULE_SETUP
-#line 11 "comment_counter.l"
-{ single_line_comments++; }  // Count single-line comments
+#line 6 "file_stats.l"
+{ spaces++; }       // Match spaces and tabs, increase space count
 	YY_BREAK
 case 2:
 YY_RULE_SETUP
-#line 13 "comment_counter.l"
-{ 
-                  BEGIN(MULTI_COMMENT);      // Enter multi-line comment state
-                  in_comment = 1;
-                  comment_line_start = 1;
-                  multi_line_comments++;     // Count first line of multi-line comment
-                }
+#line 7 "file_stats.l"
+{ words++; }        // Match words (sequences of alphanumeric characters), increase word count
 	YY_BREAK
-
 case 3:
 YY_RULE_SETUP
-#line 21 "comment_counter.l"
-{
-                  if (!comment_line_start) { // Don't double-count the start line
-                      multi_line_comments++;
-                  }
-                  comment_line_start = 0;    // Reset the start line flag
-                }
+#line 8 "file_stats.l"
+{ lines++; }        // Match newline character, increase line count
 	YY_BREAK
 case 4:
 YY_RULE_SETUP
-#line 28 "comment_counter.l"
-{
-                  BEGIN(INITIAL);            // Return to normal state
-                  in_comment = 0;
-                  comment_line_start = 0;
-                }
+#line 9 "file_stats.l"
+{ characters++; }   // Match any character (excluding newline), increase character count
 	YY_BREAK
 case 5:
 YY_RULE_SETUP
-#line 34 "comment_counter.l"
-;                           // Ignore other characters in comments
-	YY_BREAK
-
-case 6:
-YY_RULE_SETUP
-#line 37 "comment_counter.l"
-;                           // Ignore newlines outside comments
-	YY_BREAK
-case 7:
-YY_RULE_SETUP
-#line 38 "comment_counter.l"
-;                           // Ignore all other characters
-	YY_BREAK
-case 8:
-YY_RULE_SETUP
-#line 40 "comment_counter.l"
+#line 10 "file_stats.l"
 ECHO;
 	YY_BREAK
-#line 667 "lex.yy.c"
+#line 628 "lex.yy.c"
 case YY_STATE_EOF(INITIAL):
-case YY_STATE_EOF(MULTI_COMMENT):
 	yyterminate();
 
 	case YY_END_OF_BUFFER:
@@ -956,7 +916,7 @@ static yy_state_type yy_get_previous_state()
 		while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 			{
 			yy_current_state = (int) yy_def[yy_current_state];
-			if ( yy_current_state >= 17 )
+			if ( yy_current_state >= 10 )
 				yy_c = yy_meta[(unsigned int) yy_c];
 			}
 		yy_current_state = yy_nxt[yy_base[yy_current_state] + (unsigned int) yy_c];
@@ -991,11 +951,11 @@ yy_state_type yy_current_state;
 	while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 		{
 		yy_current_state = (int) yy_def[yy_current_state];
-		if ( yy_current_state >= 17 )
+		if ( yy_current_state >= 10 )
 			yy_c = yy_meta[(unsigned int) yy_c];
 		}
 	yy_current_state = yy_nxt[yy_base[yy_current_state] + (unsigned int) yy_c];
-	yy_is_jam = (yy_current_state == 16);
+	yy_is_jam = (yy_current_state == 9);
 
 	return yy_is_jam ? 0 : yy_current_state;
 	}
@@ -1550,32 +1510,35 @@ int main()
 	return 0;
 	}
 #endif
-#line 40 "comment_counter.l"
+#line 10 "file_stats.l"
 
 
+// Main function
 int main() {
+    // Open the file for reading
     printf("Enter the filename: ");
     char filename[256];
     scanf("%s", filename);
-    
     FILE *file = fopen(filename, "r");
+
     if (file == NULL) {
         printf("File not found!\n");
         return 1;
     }
 
-    yyin = file;
-    yylex();
-    
-    printf("\nComment Statistics:\n");
-    printf("Single-line comments: %d\n", single_line_comments);
-    printf("Multi-line comment lines: %d\n", multi_line_comments);
-    printf("Total comment lines: %d\n", single_line_comments + multi_line_comments);
-    
-    fclose(file);
+    yyin = file;  // Set input stream to the file
+    yylex();      // Process the file
+
+    // Print the counts
+    printf("\nCharacters: %d\n", characters);
+    printf("Words: %d\n", words);
+    printf("Spaces: %d\n", spaces);
+    printf("Lines: %d\n", lines);
+
+    fclose(file);  // Close the file after processing
     return 0;
 }
 
 int yywrap() {
-    return 1;
+    return 1;  // End of input handling
 }
